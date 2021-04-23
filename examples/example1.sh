0eea57396eb3f3a87d4905453972d42b6304244d
David Chin <david.w.h.chin@gmail.com>
2021-04-23 13:45:05 -0400

    Add all SGE env vars

--- examples/example1.sh
+++ examples/example1.sh
@@ -20,10 +20,28 @@ hello=HELLO
 
 echo $TMP
 echo $TMPDIR
+echo $hello
+
+echo $SGE_ACCOUNT
+echo $SGE_ARCH
+echo $SGE_BINARY_PATH
+echo $SGE_BINDING
+echo $SGE_CELL
+echo $SGE_CWD_PATH
+echo $SGE_JOB_SPOOL_DIR
+echo $SGE_O_HOME
+echo $SGE_O_HOST
+echo $SGE_O_LOGNAME
+echo $SGE_O_MAIL
+echo $SGE_O_PATH
+echo $SGE_O_SHELL
 echo $SGE_O_WORKDIR
-echo $SGE_TASK_ID
+echo $SGE_ROOT
+echo $SGE_STDERR_PATH
+echo $SGE_STDIN_PATH
+echo $SGE_STDOUT_PATH
 echo $SGE_TASK_FIRST
+echo $SGE_TASK_ID
 echo $SGE_TASK_LAST
 echo $SGE_TASK_STEPSIZE
 
-echo $hello
